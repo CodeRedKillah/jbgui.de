9249e2657b8383197612913df0421dd60a811917
jaschlitter@icloud.com <jaschlitter@icloud.com>
2019-07-21 02:30:00 -0700

    testing dark mode saving

---
+++ unc0ver/cookies.js
@@ -0,0 +1,22 @@
+function createCookie(cname, value, expiredays) {
+    var expiredate = new Date()
+    expiredate.setDate(expiredate.getDate() + expiredays);
+    var cvalue = escape(value) + ((expiredays == null) ? "" : "; expires=" + expiredate.toUTCString() );
+    document.cookie = cname + "=" + cvalue + "; path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
